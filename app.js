64043e89a7f2c3effc161205dd065c04a7526e80
Kefan Yang <yang.3949@buckeyemail.osu.edu>
2019-07-03 23:40:22 -0400

    index routing

--- app.js
+++ app.js
@@ -16,6 +16,29 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// MONGOOSE/MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+})
+const Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", (req, res) => {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+})
+
 app.listen(3000, () => {
     console.log("Blog App Listening on Port 3000!!!");
 })
